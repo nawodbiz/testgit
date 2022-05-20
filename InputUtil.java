c28f5e9ad72020d62d5ad3f09f642d8154ba46c3
Ishan Nawod <nawod.biz@gmail.com>
2022-05-21 01:25:33 +0530

    basic Structure created

---
+++ InputUtil.java
@@ -0,0 +1,9 @@
+public class InputUtil {
+
+    public static int getInteger(String msg){
+
+        return 0;
+
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1 = InputUtil.getInteger("Enter number 1");
+        int num2 = InputUtil.getInteger("Enter number 2");
+        int total = num1 + num2;
+        System.out.println("Total is: " + total);
+
+    }
+}
